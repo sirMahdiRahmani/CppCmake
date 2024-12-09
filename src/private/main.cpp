230262d07c0b1a3d7c518a24f7d14b3569fc9bd8
Mahdi Rahmani <sir.mahdirahmani@gmail.com>
2024-12-09 17:06:58 +0330

    change source

--- src/private/main.cpp
+++ src/private/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int a = 1, b = 2, c = 3, d = 4;
-    std::cout << "hgelli123" << std::endl;
+    std::cout << "Cmake Test" << std::endl;
+    print("Test Print");
     std::cin.get();
 }
